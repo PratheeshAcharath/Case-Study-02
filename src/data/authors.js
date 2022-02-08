649c1bd73c37e5c176aec9da367915dbe8ebe008
PratheeshAcharath <93700454+PratheeshAcharath@users.noreply.github.com>
2022-02-09 03:09:43 +0530

    Update authors.js

--- src/data/authors.js
+++ src/data/authors.js
@@ -20,6 +20,7 @@ var authors =
         "title":"J K Rowling", 
         "image":"/images/jkrowling.jpg",
         "about":"J.K. Rowling, in full Joanne Kathleen Rowling, (born July 31, 1965, Yate, near Bristol, England), British author, creator of the popular and critically acclaimed Harry Potter series, about a young sorcerer in training."
+        
     },
 
     {
